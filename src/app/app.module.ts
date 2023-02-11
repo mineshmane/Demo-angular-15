2d81333ee1e8d005e56b75c772e9c9155c2b9875
mineshmane <mineshmane94@gmail.com>
2023-02-11 17:41:11 +0530

    [Minesh mat card upadted]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatCardModule} from '@angular/material/card'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -40,7 +40,7 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,MatDialogModule,
+    AppRoutingModule,MatDialogModule,MatCardModule,
     BrowserAnimationsModule,MatFormFieldModule,MatInputModule,FormsModule,MatButtonModule,ReactiveFormsModule,HttpClientModule,
     MatSidenavModule,MatToolbarModule,MatListModule,MatIconModule
   ],
